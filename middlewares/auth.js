9b3d005c41a879593800c0a2178ab2f45301fd2a
Alex Chernov <nak4442@gmail.com>
2023-08-11 01:20:58 +0300

    Проверка на администратора

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const auth = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    console.log(`Попытка авторизации. Заголовки: `, authHeader, token);
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
@@ -14,11 +13,19 @@ const auth = (req, res, next) => {
         return res.status(403).json({ error: 'Forbidden' });
       }
       req.user = user;
-      console.log(`Вошел пользователь ${user}`)
       next();
     });
 }
 
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(403).json({ error: 'Access forbidden' });
+    }
+}
+
 module.exports = {
-    auth    
+    auth,
+    isAdmin
 }
