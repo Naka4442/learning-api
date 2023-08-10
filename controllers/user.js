6de07c9851bf12cf265a335d01e81bb518126808
Alex Chernov <nak4442@gmail.com>
2023-08-10 16:35:07 +0300

    Регистрация с хэшированием и проверкой

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-const User = require("../models/user");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 const all = async (req, res) => {
     const users = await User.find();
@@ -8,11 +9,17 @@ const all = async (req, res) => {
 const signup = async (req, res) => {
     if(!req.body) return response.sendStatus(400);
 
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        return res.status(400).json({ error: 'Пользователь с таким email уже существует' });
+    }
+
     const { email, password, name } = req.body;
-    const user = new User({ email, password, name });
+    const hashedPassword = bcrypt.hash(password, 10);
+    const user = new User({ email, password : hashedPassword, name });
     await user.save();
 
-    res.status(201).json({ message : "Пользователь успешно создан" });
+    res.status(201).json({ message : 'Пользователь успешно создан' });
 }
 
 module.exports = {
